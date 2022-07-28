8ea69a056ab08a65788f39acd2e52a3913e6f4ec
Anindya Dey <andy.ani1992@gmail.com>
2022-07-28 18:41:04 +0530

    update heading on langing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,14 +11,15 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
+        <p className="text-xl font-light">Welcome to the{' '}</p>
+        <h1 className="mt-6 text-6xl font-bold">
+          
           <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+            World of DSA!
           </a>
         </h1>
 
-        <p className="mt-3 text-2xl">
+        <p className="mt-6 text-2xl">
           Get started by editing{' '}
           <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
             pages/index.tsx
